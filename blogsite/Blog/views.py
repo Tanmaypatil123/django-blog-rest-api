f416a0c7de219586bdbc700686e64abffc677529
Tanmaypatil123 <77950208+Tanmaypatil123@users.noreply.github.com>
2022-02-27 14:45:20 +0530

    Added alternative method to genrics to make requests

--- blogsite/Blog/views.py
+++ blogsite/Blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
-
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
 from .models import Blog
 from .serializers import BlogSerializers
+from rest_framework.response import Response
 # Create your views here.
 class BlogDetailAPIView(generics.RetrieveAPIView):
     queryset = Blog.objects.all()
@@ -43,3 +45,28 @@ class BlogCreateListAPIView(generics.ListCreateAPIView):
         serializer.save(content =content)
 
 blog_create_list_view = BlogCreateListAPIView.as_view()        
+
+@api_view(["GET","POST"])
+def blog_all_view(request,pk=None,*args,**kwargs):
+    method = request.method
+
+    if method =="GET":
+        if pk is not None:
+            obj = get_object_or_404(Blog,pk=pk)
+            data = BlogSerializers(obj,many=False)
+            return Response(data)
+        queryset = Blog.objects.all()
+        data = BlogSerializers(queryset,many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = BlogSerializers(data = request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None   
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)     
