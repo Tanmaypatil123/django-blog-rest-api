20fe015fb6e1f6edb3b65855d46eda11ced75c6e
Tanmaypatil123 <77950208+Tanmaypatil123@users.noreply.github.com>
2022-02-28 16:24:56 +0530

    added authentication folder

---
+++ blogsite/api/authentication.py
@@ -0,0 +1,6 @@
+from rest_framework.authentication import TokenAuthentication as BaseTokenAuth
+
+from rest_framework.authtoken.models import Token
+
+class TokenAuthentication(BaseTokenAuth):
+    keyword = 'Bearer'
